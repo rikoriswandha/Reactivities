72dbfd3d05d58649fb447a0b99ab51dc0c5f4cab
Riko Riswandha <rikorisw@gmail.com>
2020-12-06 17:32:10 +0800

    Add walking skeleton for client

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,6 +32,13 @@ namespace API
       {
         opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
       });
+      services.AddCors(opt =>
+      {
+        opt.AddPolicy("CorsPolicy", policy =>
+        {
+          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+        });
+      });
       services.AddControllers();
       services.AddSwaggerGen(c =>
       {
@@ -54,6 +61,8 @@ namespace API
       app.UseRouting();
 
       app.UseAuthorization();
+
+      app.UseCors("CorsPolicy");
 
       app.UseEndpoints(endpoints =>
       {
